33b46d7b4141f4313d69dfa3f16e88dda2c90779
MiguelLopez06 <30665774+MiguelLopez06@users.noreply.github.com>
2017-10-30 21:00:28 -0500

    este

---
+++ DetermineAurora.py
@@ -0,0 +1,16 @@
+from AuroraCreator import WaterAuroraCreator
+from AuroraCreator import FireAuroraCreator
+from AuroraCreator import DevilAuroraCreator
+from AuroraCreator import PsychicAuroraCreator
+
+class DetermineAurora():
+    def __init__(self):
+        pass
+    createaurora=None
+    aurora=None
+    def createAurora(self, number):
+        self.createaurora={1: WaterAuroraCreator(), 2: FireAuroraCreator(), 3: DevilAuroraCreator(), 4: PsychicAuroraCreator() }
+        self.aurora = self.createaurora[number].createAurora()
+        print(str(self.aurora))
+    def getAurora(self):
+        return self.aurora
